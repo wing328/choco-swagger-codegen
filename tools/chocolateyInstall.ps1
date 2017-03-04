67576031f533ae338f869feb12fc0b1b6dcca692
wing328 <wing328@yahoo.com>
2017-03-04 23:43:44 +0800

    add nuspec, fix ps1

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -3,18 +3,18 @@ $package = Split-Path $tools
 $content = Join-Path $package 'swagger-codegen-2.2.2'
 $bat = Join-Path $tools 'swagger-codegen.bat'
  
-Install-ChocolateyPackage `
+Install-ChocolateyZipPackage `
     -PackageName 'swagger-codegen' `
     -Url 'https://raw.githubusercontent.com/wing328/choco-swagger-codegen/master/zipped_jar/swagger-codegen-cli-2.2.2.zip' `
     -Checksum '52a3b9ee47ba1bd8caf69bb0592554e6e38a598ad2069902f8a5c450c740e23f' `
     -ChecksumType 'SHA256' `
-    -UnzipLocation $package
+    -UnzipLocation $content
  
 Install-ChocolateyEnvironmentVariable `
     -VariableName 'SWAGGER_CODEGEN_HOME' `
     -VariableValue $content `
     -VariableType 'Machine'
- 
+
 Install-BinFile `
     -Name 'swagger-codegen' `
     -Path $bat
